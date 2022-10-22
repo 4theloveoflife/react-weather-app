bd103a19057179d05b38d03d7b48d47be47f817d
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-10-22 20:45:05 +0900

    added GitHub repository url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-     Weather App
-    </div>
-  );
+export default function App() {
+  return  <div className="App">
+   <h1>Weather App</h1> 
+    <footer>
+This project was coded by Tiffany Walker and is {" "}
+    <a href="https://github.com/4theloveoflife/react-weather-app.git"
+    target="_blank">
+ open sourced on GitHub
+    </a>
+        </footer>
+    </div> ;
 }
