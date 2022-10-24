52f2fdc891d9fb2898c357c1cbc6070c987d458c
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-10-24 20:59:01 +0900

    installed bootstrap
    installed bootstrap on index.js
    added container to app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return  <div className="App">
-   <h1>Weather App</h1> 
+  return(
+      <div className="App">
+               <div className="container">
+                  <h1>Weather App</h1> 
     <footer>
 This project was coded by Tiffany Walker and is {" "}
     <a href="https://github.com/4theloveoflife/react-weather-app.git"
@@ -11,5 +13,7 @@ This project was coded by Tiffany Walker and is {" "}
  open sourced on GitHub
     </a>
         </footer>
-    </div> ;
+    </div>
+    </div>
+    ) ;
 }
