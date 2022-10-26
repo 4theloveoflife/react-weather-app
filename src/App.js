12852435c600ee1a6897ba3598af2547f4f12a1d
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-10-26 23:04:33 +0900

    added api integration

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import "./App.css";
 import Weather from "./Weather";
+import "./App.css";
+
 
 export default function App() {
   return(
       <div className="App">
                <div className="container">
-                <Weather />
+                <Weather  defaultCity="Tokyo"/>
   
     <footer>
 This project was coded by Tiffany Walker and is {" "}
-    <a href="https://github.com/4theloveoflife/react-weather-app.git"
-    target="_blank">
+    <a 
+    href="https://github.com/4theloveoflife/react-weather-app.git"
+    target= "_blank"
+    rel="noopener noreferrer">
  open sourced on GitHub
     </a>
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
-export default function Weather(){
+export default function Weather(props){
+    const[ weatherData, setWeatherData]=useState({ready: false});
+    function handleResponse(response){
+        console.log(response.data);
+        setWeatherData({
+            ready:true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            date: "Wednesday 07:00",
+            description: response.data.weather[0].description,
+            iconUrl: "http://openweathermap.org/img/wn/10n@2x.png",
+            wind: response.data.wind.speed,
+            city: response.data.name
+        });
+    }
+    if (weatherData.ready){
     return (
          <div className="Weather">
             <form>
@@ -10,37 +26,49 @@ export default function Weather(){
                         type="search"
                         placeholder="Enter City.."
                         className="form-control"
+                        autoFocus="on"
                         />
                         </div>
                         <div className="col-3">
                             <input 
-                            type="submit" value="Search" className="btn btn-primary"/>
+                            type="submit" value="Search" className="btn btn-primary w-100"/>
                         </div>
                     </div>
                       </form>
                
-        <h1>Tokyo</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
-            <li>Monday 22:00</li>
-            <li>Mostly Cloudy</li>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize"> {weatherData.description}</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
             <div className="col-6">
+                <div className="clearfix">
                 <img
-                src="http://openweathermap.org/img/wn/10n@2x.png"
-                alt="light rain"
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left"
                 />
-                18℃
+                <div className="float-left">
+                <span className="temperature">{Math.round(weatherData.temperature)}</span>
+                <span className="unit">℃</span>
+            </div>
+            </div>
             </div>
             <div className="col-6">
                 <ul>
-                    <li>Precipitation:15%</li>
-                    <li>Humidity:10%</li>
-                    <li>Wind:8km/h</li>
+                    <li>Humidity:{weatherData.humidity}%</li>
+                    <li>{weatherData.wind}km/h</li>
                 </ul>
             </div>
         </div>
         </div>
-
     );
+} else{
+    const apiKey="b1ad2579aaffa4376d245595812d15f0";
+    let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${props.defaultcity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
