e986563fe9f0ac49b8af2eb7d05be149ce130d14
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-11-03 23:33:54 +0900

    fixed api url

--- src/Weather.js
+++ src/Weather.js
@@ -29,7 +29,7 @@ export default function Weather(props){
     }
     function search(){
           const apiKey="b1ad2579aaffa4376d245595812d15f0";
-          let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+          let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
           axios.get(apiUrl).then(handleResponse);
     }
 
