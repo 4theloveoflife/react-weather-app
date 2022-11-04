a58f73ea633cce3b271ffbcf5e7bffc7b5dd74fb
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-11-04 20:43:29 +0900

    added forecast component
    added forecast java script and css components
    changed icon call code to props.size when calling

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -28,7 +28,7 @@ export default function WeatherIcon(props){
         <ReactAnimatedWeather
         icon={codeMapping[props.code]}
        color= "#2E4053"
-       size={38}
+       size={props.size}
        animate={true}
   />
     );
