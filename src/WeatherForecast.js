e55655aed76d67c44d999d204ffaa9b9891ed275
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-11-05 01:13:01 +0900

    called setFunction

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,7 +8,7 @@ export default function WeatherForecast(props){
     let [forecast, setForecast]=useState(null);
 
     function handleResponse(response){
-        console.log(response.data);
+        setForecast(response.data.daily);
         setloaded(true);
     }
 
