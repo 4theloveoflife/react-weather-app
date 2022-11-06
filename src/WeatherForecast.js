128068e4f9a960ca4db6312d2fca4e2bab5a47d4
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-11-06 17:38:23 +0900

    included useEffect
    fixed the spelling of corrdinates

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -6,6 +6,11 @@ import axios from "axios";
 export default function WeatherForecast(props){
     let [loaded, setLoaded]=useState(false);
     let [forecast, setForecast]=useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    },  [props.coordinates]);
+    
 
     function handleResponse(response){
         setForecast(response.data.daily);
@@ -31,11 +36,11 @@ return(
    
 }else{
     let apiKey="b1ad2579aaffa4376d245595812d15f0";
-    let longitude=props.coordineates.lon;
+    let longitude=props.coordinates.lon;
     let latitude=props.coordinates.lat;
     let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-    
+
 return null;
 }
 }
