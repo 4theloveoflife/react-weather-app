ffc81748fdd7eb0cfbb0e1dfb61bbc439bc36e33
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-11-06 13:14:24 +0900

    added spaces for appeal and used proper structure for setLoaded

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,26 +1,26 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props){
-    let [loaded, setloaded]=useState(false);
+    let [loaded, setLoaded]=useState(false);
     let [forecast, setForecast]=useState(null);
 
     function handleResponse(response){
         setForecast(response.data.daily);
-        setloaded(true);
+        setLoaded(true);
     }
 
     if (loaded) {
 return(
     <div className="WeatherForecast">
         <div className="row">
-            {forecast.map(function  (dailyForecast,index) {
-                if (index< 5) {
+            {forecast.map(function  (dailyForecast, index) {
+                if (index <  5) {
                 return(
-                       <div className="col" key={index}>
-                <WeatherForecastDay data={dailyForecast} />
+                 <div className="col" key={index}>
+                    <WeatherForecastDay data={dailyForecast} />
                         </div>
                  );
             }
